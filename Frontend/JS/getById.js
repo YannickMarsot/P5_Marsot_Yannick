a9f9c0034464e7ac20d4db3659e58fa3fa44b85e
Yannick <yannmarsot@hotmail.fr>
2021-09-18 12:34:11 +0200

    Ajout style + bouton dropdown

--- Frontend/JS/getById.js
+++ Frontend/JS/getById.js
@@ -1,20 +1,35 @@
+const URL = "http://localhost:3000/api/cameras/?getById=${._id}";
+const a = window.document.createElement("a");
+a.href = a;
+let params = URLSearchParams(a.search);
+params.get("/?getById=${._id}");
+
 function getArticleById() {
-  fetch("http://localhost:3000/api/cameras/?getById=${article._id}")
+  fetch("http://localhost:3000/api/cameras/?getById=${._id}")
     .then(function (res) {
       if (res.ok) {
         return res.json();
       }
     })
-    .then(function (article) {
+    .then(function (_id) {
       console.log("coucou");
       document.querySelector(".list-product").innerHTML = `
-          <div class="col-4 mx-auto grandConteneur" id="${article._id}">
+          <div class="col-4 mx-auto grandConteneur" id="${_id}">
           <div class="image">
-          <img src="${article.imageUrl}">
-          <p class="lenses">${article.lenses}</p>
-          <p class="name">${article.name}</p>
-          <p class="price">${article.price}
-          <p class="description">${article.description}</p>
+          <img src="${.imageUrl}">
+          <p class="lenses"><div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+          Lenses
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          <li><a class="dropdown-item" href="#">${.lenses}</a></li>
+          <li><a class="dropdown-item" href="#">${.lenses}</a></li>
+          <li><a class="dropdown-item" href="#">${.lenses}</a></li>
+        </ul>
+        </div></p>
+          <p class="name">${.name}</p>
+          <p class="price">${.price}
+          <p class="description">${.description}</p>
           `;
     });
 }
