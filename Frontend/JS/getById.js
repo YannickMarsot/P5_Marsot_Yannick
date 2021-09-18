ea6b9a1a2b0ef17bd85ea8ebaa1a7d3f3295920b
Yannick <yannmarsot@hotmail.fr>
2021-09-18 12:03:04 +0200

    Ajout fonction "getArticleBId"
    (ne marche pas correctement)

--- Frontend/JS/getById.js
+++ Frontend/JS/getById.js
@@ -1,32 +1,20 @@
-const URL ="http://localhost:3000/api/cameras/?getById=${article._id}"
-
-const a = window.document.createElement("a")
-a.href = URL
-let params = new URLSearchParams(a.search)
-params.get = (getById) //? ${article._id};
-
 function getArticleById() {
   fetch("http://localhost:3000/api/cameras/?getById=${article._id}")
-  .then(
-    function (res) {
+    .then(function (res) {
       if (res.ok) {
         return res.json();
       }
-    }
-  );
-  .then(function (productbyId) {
-console.log("coucou");
-document.querySelector(".list-product").innerHTML `
-        <div class="col-4 mx-auto grandConteneur" id="${article._id}">
-        <div class="image">
-        <a href="http://127.0.0.1:5500/Frontend/${article._id}.html">
-        <img src="${article.imageUrl}">
-        <p class="lenses">${article.lenses}</p>
-        <p class="name">${article.name}</p>
-        <p class="price">${article.price}
-        <p class="description">${article.description}</p>
-        </a>
-        `;
-      }
-  )
+    })
+    .then(function (article) {
+      console.log("coucou");
+      document.querySelector(".list-product").innerHTML = `
+          <div class="col-4 mx-auto grandConteneur" id="${article._id}">
+          <div class="image">
+          <img src="${article.imageUrl}">
+          <p class="lenses">${article.lenses}</p>
+          <p class="name">${article.name}</p>
+          <p class="price">${article.price}
+          <p class="description">${article.description}</p>
+          `;
+    });
 }
