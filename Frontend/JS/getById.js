478d4d1e49355bcec15bda9d4dcfa0a2fd531a46
Yannick <yannmarsot@hotmail.fr>
2021-10-10 14:17:06 +0200

    Fonction getById + notes

--- Frontend/JS/getById.js
+++ Frontend/JS/getById.js
@@ -8,26 +8,29 @@ function get_article_by_id() {
   //fonction pour afficher l'article souhaiter avec le parametre URL
   const id = getParameter("id");
   console.log(id);
-
+  var produit = "";
   fetch("http://localhost:3000/api/cameras/" + id).then((res) => {
-    console.log(res.data);
+    produit = res.json();
+
+    console.log(produit);
+    //console.log(res.data);
     document.querySelector(".list-product").innerHTML = `
-      <div class="col-4 mx-auto grandConteneur" id="${id._id}">
+      <div class="col-4 mx-auto grandConteneur" id="${produit._id}">
       <div class="image">
-      <img src="${id.imageUrl}">
+      <img src="${produit.imageUrl}">
       <p class="lenses"><div class="dropdown">
     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
       Lenses
     </button>
     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-      <li><a class="dropdown-item" href="#">${id.lenses}</a></li>
-      <li><a class="dropdown-item" href="#">${id.lenses}</a></li>
-      <li><a class="dropdown-item" href="#">${id.lenses}</a></li>
+      <li><a class="dropdown-item" href="#">${produit.lenses}</a></li>
+      <li><a class="dropdown-item" href="#">${produit.lenses}</a></li>
+      <li><a class="dropdown-item" href="#">${produit.lenses}</a></li>
     </ul>
     </div></p>
-      <p class="name">${id.name}</p>
-      <p class="price">${id.price}
-      <p class="description">${id.description}</p>
+      <p class="name">${produit.name}</p>
+      <p class="price">${produit.price}
+      <p class="description">${produit.description}</p>
       `;
   });
 }
