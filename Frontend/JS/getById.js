a28752649f85ead468e16e57480447e75804c27a
Yannick <yannmarsot@hotmail.fr>
2021-10-16 23:15:38 +0200

    Correction Inner HTML et Sass

--- Frontend/JS/getById.js
+++ Frontend/JS/getById.js
@@ -57,31 +57,30 @@ function get_article_by_id() {
 
 function write_product(product) {
   document.querySelector(".list-product").innerHTML = `
-      <div class="col-4 mx-auto grandConteneur" id="${product._id}">
-        <div class="image">
-          <img src="${product.imageUrl}">
-        </div>
-      <p class="lenses">
-        <div class="dropdown">
-          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-              Lenses
-          </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li><a class="dropdown-item" href="#">${product.lenses}</a></li>
-            <li><a class="dropdown-item" href="#">${product.lenses}</a></li>
-            <li><a class="dropdown-item" href="#">${product.lenses}</a></li>
-          </ul>
-        </div></p>
-        <div class="petitConteneur">
-          <p class="name">${product.name}</p>
-          <p class="price">${product.price}
-          <p class="description">${product.description}</p>
-          <a href="#" title="add to cart" class="addToCart">
-            <p>Ajouter au panier</p>
-          <a/>
-        </div>
+    <div class="col-4 mx-auto grandConteneur" id="${product._id}">
+      <div class="image">
+        <img src="${product.imageUrl}">
       </div>
+      <div class="lensesConteneur">
+        <label for="lenses">Choose a lense</label>
+          <select name="Lenses" id="lenses-select">
+          <option value="exemple">"exemple 1"</option>
+          <option value="exemple">"exemple 2"</option>
+          <option value="exemple">"exemple 3"</option>
+          </select>
+      </div>
+      <div class="petitConteneur">
+        <p class="name">${product.name}</p>
+        <p class="price">${product.price}</p>
+        <p class="description">${product.description}</p>
+        <a href="#" title="add to cart" class="addToCart">
+          <p>Ajouter au panier</p>
+        <a/>
+      </div>
+    </div>
       `;
 }
 
 get_article_by_id();
+
+//dropdown menu "lenses"S
--- Frontend/JS/listProduct.js
+++ Frontend/JS/listProduct.js
@@ -14,29 +14,17 @@ function getArticle() {
         document.querySelector(".list-product").innerHTML += `
         <div class="conteneur" id="${article._id}">
           <div class="image">
-          <a href="http://127.0.0.1:5500/Frontend/produits.html?id=${article._id}">
-            <img src="${article.imageUrl}">
+            <a href="http://127.0.0.1:5500/Frontend/produits.html?id=${article._id}">
+              <img src="${article.imageUrl}">
+            </a>
           </div>
-          </a>
-          <p class="lenses">
-            <div class="dropdown">
-              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                 Lenses
-              </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                  <li><a class="dropdown-item" href="#">${article.lenses}</a></li>
-                  <li><a class="dropdown-item" href="#">${article.lenses}</a></li>
-                  <li><a class="dropdown-item" href="#">${article.lenses}</a></li>
-                </ul>
-            </div>
-            </p>
-            <div class="petitConteneur">
-              <p class="name">${article.name}</p>
-              <p class="price">${article.price}
-              <p class="description">${article.description}</p>
-              <a href="#" title="add to cart" class="addToCart"><p>Ajouter au panier</p>
-              <a/>
-            </div>
+          <div class="petitConteneur">
+            <p class="name">${article.name}</p>
+            <p class="price">${article.price}
+            <p class="description">${article.description}</p>
+            <a href="#" title="add to cart" class="addToCart"><p>Ajouter au panier</p>
+            <a/>
+          </div>
         </div>
         `;
         //utilisation des "templates litteral" pour selectionner
