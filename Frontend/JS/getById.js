de4d94955fe3802a611ac42b54cebe6c119260b6
Yannick <yannmarsot@hotmail.fr>
2021-10-17 16:14:09 +0200

    dropdown menu

--- Frontend/JS/getById.js
+++ Frontend/JS/getById.js
@@ -1,43 +1,3 @@
-// function getParameter(parameterName) {
-//   //fonction pour récupérer le paramétre d'une URL
-//   let parameter = new URLSearchParams(window.location.search);
-//   return parameter.get(parameterName);
-// }
-
-// function get_article_by_id() {
-//   //fonction pour afficher l'article souhaiter avec le parametre URL
-//   const id = getParameter("id");
-//   console.log(id);
-//   var produit = "";
-//   fetch("http://localhost:3000/api/cameras/" + id)
-//   .then((res) => {
-//     produit = res.json();
-
-//     console.log(produit);
-//     //console.log(res.data);
-//     document.querySelector(".list-product").innerHTML = `
-//       <div class="col-4 mx-auto grandConteneur" id="${produit._id}">
-//       <div class="image">
-//       <img src="${produit.imageUrl}">
-//       <p class="lenses"><div class="dropdown">
-//     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-//       Lenses
-//     </button>
-//     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-//       <li><a class="dropdown-item" href="#">${produit.lenses}</a></li>
-//       <li><a class="dropdown-item" href="#">${produit.lenses}</a></li>
-//       <li><a class="dropdown-item" href="#">${produit.lenses}</a></li>
-//     </ul>
-//     </div></p>
-//       <p class="name">${produit.name}</p>
-//       <p class="price">${produit.price}
-//       <p class="description">${produit.description}</p>
-//       `;
-//   });
-// }
-
-// get_article_by_id();
-
 function getParameter(parameterName) {
   //fonction pour récupérer le paramétre d'une URL
   let parameter = new URLSearchParams(window.location.search);
@@ -56,6 +16,10 @@ function get_article_by_id() {
 }
 
 function write_product(product) {
+  console.log(product.lenses);
+  product.lenses.forEach(function (item, index) {
+    console.log(item, index);
+  });
   document.querySelector(".list-product").innerHTML = `
     <div class="col-4 mx-auto grandConteneur" id="${product._id}">
       <div class="image">
@@ -64,9 +28,9 @@ function write_product(product) {
       <div class="lensesConteneur">
         <label for="lenses">Choose a lense</label>
           <select name="Lenses" id="lenses-select">
-          <option value="exemple">"exemple 1"</option>
-          <option value="exemple">"exemple 2"</option>
-          <option value="exemple">"exemple 3"</option>
+          <option value="exemple">${product.lenses[0]}</option>
+          <option value="exemple">${product.lenses[1]}</option>
+          <option value="exemple">${product.lenses[2]}</option>
           </select>
       </div>
       <div class="petitConteneur">
@@ -82,5 +46,3 @@ function write_product(product) {
 }
 
 get_article_by_id();
-
-//dropdown menu "lenses"S
