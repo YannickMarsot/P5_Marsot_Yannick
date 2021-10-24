0fef5d96dba6595feda834b41b27f7df0904116a
Yannick <yannmarsot@hotmail.fr>
2021-10-24 16:50:40 +0200

    ajout en commentaires des potentiels correctifs

--- Frontend/JS/getById.js
+++ Frontend/JS/getById.js
@@ -20,6 +20,16 @@ function write_product(product) {
   product.lenses.forEach(function (item, index) {
     console.log(item, index);
   });
+  //version 2 (non fonctionnelle)
+  //avec display "none" pour "undefined"
+  // console.log(product.lenses);
+  // if (product.lenses !== "undefined") {
+  //   product.lenses = '<p style="display: none">undefined</p>';
+  // } else {
+  //   product.lenses.forEach(function (item, index) {
+  //     console.log(item, index);
+  //   });
+  // }
   document.querySelector(".list-product").innerHTML = `
     <div class="col-4 mx-auto grandConteneur" id="${product._id}">
       <div class="image">
@@ -44,6 +54,8 @@ function write_product(product) {
     </div>
       `;
 
+  //fonction à mettre dans "write_product"
+  //car product.name non défini sinon?
   // .then(function addToCart() {
   //   let name = document.getElementsByClassName("name");
   //   let price = document.getElementsByClassName("price");
@@ -57,12 +69,21 @@ function write_product(product) {
 
 //ajout au panier
 function addToCart() {
+  // let id = document.getElementById("id");
   let name = document.getElementsByClassName("name");
   let price = document.getElementsByClassName("price");
   let image = document.getElementsByClassName("image");
   console.log(name, price, image);
   let product = { name, no: 1 };
-  let panier = { price, no: 1 };
+  //let product= {
+  //   id,
+  //   name,
+  //   no: 1
+  // }
+  let panier = {
+    price,
+    no: 1,
+  };
   localStorage.setItem(JSON.stringify(product), JSON.stringify(panier));
 }
 
