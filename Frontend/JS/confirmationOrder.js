9f2308dad23e263852adead278b0f32bf6eeb4ad
Yannick <Yannmarsot@hotmail.fr>
2022-03-09 16:19:34 +0100

    lien page confirmation

--- Frontend/JS/confirmationOrder.js
+++ Frontend/JS/confirmationOrder.js
@@ -6,11 +6,13 @@ function getParameter(parameterName) {
 
 function getOrderId() {
   //fonction pour afficher l'article souhaiter avec le parametre URL
-  const id = getParameter("orderId");
-
-  fetch("http://localhost:3000/api/cameras/" + orderId)
+  const orderId = getParameter("orderId");
+  console.log(orderId);
+  fetch("http://http://127.0.0.1:5500/Frontend/confirmation.html" + orderId)
     .then((res) => res.json())
     .then((res) => {
       write_product(res);
     });
 }
+
+getOrderId();
